0fa77d91c48f6e35454224e4b4cd6c3fc8e92a8e
Diogo Ferreira <diogo@underdev.org>
2021-02-28 16:57:33 -0500

    mt6580: Add an RGB configuration sys attr driver for videox/corr10 Adds the RGB configuration node which leverages the gamma subsystem to perform color correction.
    The new node takes rgb triplets that range from 0-2000 each, computes
    the gamma registers and dispatches them to the gamma correction
    subsystem.

--- drivers/misc/mediatek/video/common/corr10/ddp_gamma.c
+++ drivers/misc/mediatek/video/common/corr10/ddp_gamma.c
@@ -182,10 +182,19 @@ static int disp_gamma_set_lut(const DISP_GAMMA_LUT_T __user *user_gamma_lut, voi
 		return -EFAULT;
 	}
 
+#ifdef CONFIG_MTK_VIDEOX_LIVEDISPLAY
+	if (virt_addr_valid(user_gamma_lut))
+		memcpy(gamma_lut, user_gamma_lut, sizeof(DISP_GAMMA_LUT_T));
+	else
+#endif
+
 	if (copy_from_user(gamma_lut, user_gamma_lut, sizeof(DISP_GAMMA_LUT_T)) != 0) {
 		ret = -EFAULT;
+        GAMMA_ERR("disp_gamma_set_lut: cannot copy from user mem");
 		kfree(gamma_lut);
-	} else {
+	}
+
+	if (!ret) {
 		id = gamma_lut->hw_id;
 		if (0 <= id && id < DISP_GAMMA_TOTAL) {
 			mutex_lock(&g_gamma_global_lock);
